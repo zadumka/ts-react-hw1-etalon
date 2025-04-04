e39c3b3f77d947381e1277771006fcefa19d47d8
zadumka <90329284+zadumka@users.noreply.github.com>
2025-04-04 10:14:29 +0300

    Update task-1.ts

--- src/task-1.ts
+++ src/task-1.ts
@@ -5,6 +5,3 @@ const isOnline: boolean = true;
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
 console.log(`Online: ${isOnline}`);
-const name: string = "Alice";
-const age: number = 30;
-const isOnline: boolean = true;
